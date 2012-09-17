8fafad1a0c79b25f687b45cffabe1614060cecea
Gitte <gitte.vermeiren@gmail.com>
2012-09-17 16:49:16 +0200

    Pushing new changes

--- source/prep/collections/Where.cs
+++ source/prep/collections/Where.cs
@@ -7,10 +7,46 @@ namespace prep.collections
 
   public class Where<ItemToMatch>
   {
-    public static void has_a(StudioAccessor accessor)
+    public static IEqualizer<Movie> has_a(StudioAccessor accessor)
     {
-      throw new NotImplementedException();
+        return new Equalizer(accessor);
     }
   }
 
+    public interface IEqualizer<T>
+    {
+        IMatchAn<Movie> equal_to(ProductionStudio a_production_studio);
+    }
+
+    public class Equalizer : IEqualizer<Movie>
+    {
+        private StudioAccessor _accessor;
+
+        public Equalizer(StudioAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        public IMatchAn<Movie> equal_to(ProductionStudio a_production_studio)
+        {
+            return new EqualMatch(_accessor, a_production_studio);
+        }
+    }
+
+    public class EqualMatch : IMatchAn<Movie>
+    {
+        private StudioAccessor _accessor;
+        private ProductionStudio _studio;
+
+        public EqualMatch(StudioAccessor accessor, ProductionStudio aProductionStudio)
+        {
+            _accessor = accessor;
+            _studio = aProductionStudio;
+        }
+
+        public bool matches(Movie item)
+        {
+            return _accessor(item) == _studio;
+        }
+    }
 }
