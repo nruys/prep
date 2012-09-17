f20889a3db1ec9084fb8adf58e0e2ff0ab768630
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2012-09-17 08:34:59 -0600

    Updated

--- source/prep/collections/Where.cs
+++ source/prep/collections/Where.cs
@@ -1,7 +1,23 @@
-﻿namespace prep.collections
+﻿using prep.utility;
+
+namespace prep.collections
 {
+  public delegate ProductionStudio StudioAccessor(Movie movie);
+
   public class Where<ItemToMatch>
   {
-     
+    public static StudioAccessor has_a(StudioAccessor accessor)
+    {
+      return accessor;
+    }
+  }
+
+  public static  class StudioAccessorExtensions
+  {
+    public static IMatchAn<Movie> equal_to(this StudioAccessor accessor,
+                                           ProductionStudio value)
+    {
+      return new ConditionalMatch<Movie>(x => x.production_studio == value);
+    }
   }
 }
