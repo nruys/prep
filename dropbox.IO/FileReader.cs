efdab0676c67e4c2d2ca8e3a4ff9ead7dd1bbfd5
nruys <niels.ruys@exsertus.be>
2012-09-18 10:03:23 +0200

    Pushing new changes

---
+++ dropbox.IO/FileReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace dropbox.IO
+{
+    public class FileReader
+    {
+        private readonly String _filePath;
+        private String[] _lines;
+
+        public FileReader(String filePath)
+        {
+            _filePath = filePath;
+            _lines = null;
+        }
+
+        public bool ReadFile()
+        {
+            _lines = File.ReadAllLines(_filePath);
+
+            return _lines.Any();
+        }
+
+        public Int32 GetNrOfLines()
+        {
+            return Int32.Parse(_lines[0]);
+        }
+
+        public String ReadLine(Int32 index)
+        {
+            return _lines[index];
+        }
+    }
+}
---
+++ dropbox.IO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dropbox.IO
+{
+    class Program
+    {
+        public const string _filePath = "input.txt";
+
+        static void Main(string[] args)
+        {
+            var fileReader = new FileReader(_filePath);
+            if (fileReader.ReadFile())
+            {
+                for (var i = 0; i < fileReader.GetNrOfLines(); i++)
+                {
+                    var line = fileReader.ReadLine(i);
+                }
+            }
+        }
+    }
+}
